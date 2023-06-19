8c647db53968e95ade2f38863f398163b9bcce4d
NigarMahmudova <nigarem@code.edu.az>
2023-06-19 00:00:29 -0700

    added AdminPanel

---
+++ PustokBookStore/PustokBookStore/Areas/Manage/Controllers/AuthorController.cs
@@ -0,0 +1,84 @@
+﻿using Humanizer.Localisation;
+using Microsoft.AspNetCore.Mvc;
+using PustokBookStore.DAL;
+using PustokBookStore.Entities;
+
+namespace PustokBookStore.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class AuthorController : Controller
+    {
+        private readonly PustokDbContext _context;
+        public AuthorController(PustokDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Author> model = _context.Authors.ToList();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Author author)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (_context.Authors.Any(x => x.FullName == author.FullName))
+            {
+                ModelState.AddModelError("FullName", "FullName is already taken.");
+                return View();
+            }
+
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Author author = _context.Authors.FirstOrDefault(x => x.Id == id);
+
+            if (author == null)
+            {
+                return View("Error");
+            }
+
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (_context.Authors.Any(x => x.FullName == author.FullName))
+            {
+                ModelState.AddModelError("FullName", "FullName is already taken");
+                return View();
+            }
+
+            Author existAuthor = _context.Authors.FirstOrDefault(x =>x.Id == author.Id);
+
+            if (existAuthor == null)
+            {
+                return View("Error");
+            }
+            existAuthor.FullName = author.FullName;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PustokBookStore/PustokBookStore/Areas/Manage/Controllers/GenreController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokBookStore.DAL;
+using PustokBookStore.Entities;
+
+namespace PustokBookStore.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class GenreController : Controller
+    {
+        private readonly PustokDbContext _context;
+        public GenreController(PustokDbContext context)
+        {
+            _context= context;
+        }
+        public IActionResult Index()
+        {
+           List<Genre> model = _context.Genres.Include(x=>x.Books).ToList();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Genre genre)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if(_context.Genres.Any(x=> x.Name == genre.Name))
+            {
+                ModelState.AddModelError("Name", "Name is already taken.");
+                return View();
+            }
+            _context.Genres.Add(genre);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Genre genre = _context.Genres.FirstOrDefault(x=>x.Id == id);
+
+            if(genre == null)
+            {
+                return View("Error");
+            }
+
+            return View(genre);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Genre genre)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            if (_context.Genres.Any(x => x.Name == genre.Name))
+            {
+                ModelState.AddModelError("Name", "Name is already taken.");
+                return View();
+            }
+
+            Genre existGenre = _context.Genres.FirstOrDefault(x=>x.Id==genre.Id);
+
+            if(existGenre == null)
+            {
+                return View("Error");
+            }
+
+            existGenre.Name = genre.Name;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
